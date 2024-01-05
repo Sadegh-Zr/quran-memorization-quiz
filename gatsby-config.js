d69b32c229d0f279a38408ef3da0a722137ec996
Sadegh-Zr <93543701+Sadegh-Zr@users.noreply.github.com>
2024-01-05 13:19:23 +0330

    Update SiteURL

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://sadegh-zr.github.io/quran-memorization-quiz`,
   },
   pathPrefix: "/quran-memorization-quiz",
   plugins: [],
